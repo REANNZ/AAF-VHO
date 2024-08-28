9125253768cc0adf671343950c6e46c7f24b10ab
Jamie Getty <jamie.getty@reannz.co.nz>
2024-08-28 19:46:43 +1200

    new: Remove all Grails pages and add simple page

--- second-wind/virtualhome/grails-app/controllers/virtualhome/UrlMappings.groovy
+++ second-wind/virtualhome/grails-app/controllers/virtualhome/UrlMappings.groovy
@@ -3,13 +3,7 @@ package virtualhome
 class UrlMappings {
 
     static mappings = {
-        "/$controller/$action?/$id?(.$format)?"{
-            constraints {
-                // apply constraints here
-            }
-        }
-
-        "/"(view:"/index")
+        "/"(controller:'helloworld')
         "500"(view:'/error')
         "404"(view:'/notFound')
     }
---
+++ second-wind/virtualhome/src/test/groovy/virtualhome/HelloworldControllerSpec.groovy
@@ -0,0 +1,18 @@
+package virtualhome
+
+import grails.testing.web.controllers.ControllerUnitTest
+import spock.lang.Specification
+
+class HelloworldControllerSpec extends Specification implements ControllerUnitTest<HelloworldController> {
+
+    def setup() {
+    }
+
+    def cleanup() {
+    }
+
+    void "test something"() {
+        expect:"fix me"
+            true == false
+    }
+}
