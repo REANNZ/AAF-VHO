5b78e75a43d52a93bac23910bf2d5b16fbf3283c
Jamie Getty <jamie.getty@reannz.co.nz>
2024-09-10 09:36:46 +1200

    new: Remove obsolete beans

--- update/virtualhome/grails-app/conf/spring/resources.groovy
+++ update/virtualhome/grails-app/conf/spring/resources.groovy
@@ -1,24 +1,4 @@
 beans = {
-    // Grails Application Factory Bean
-    grailsApplication(org.codehaus.groovy.grails.commons.GrailsApplicationFactoryBean) {
-        description = "Grails application factory bean"
-        grailsDescriptor = "/WEB-INF/grails.xml"
-    }
-
-    // Plugin Manager Bean
-    pluginManager(org.codehaus.groovy.grails.plugins.GrailsPluginManagerFactoryBean) {
-        description = "A bean that manages Grails plugins"
-        grailsDescriptor = "/WEB-INF/grails.xml"
-        application = ref("grailsApplication")
-    }
-
-    // Grails Runtime Configurator Bean
-    grailsConfigurator(org.codehaus.groovy.grails.commons.spring.GrailsRuntimeConfigurator) {
-        pluginManager = ref("pluginManager")
-        // Constructor arg
-        constructorArgs = [ref("grailsApplication")]
-    }
-
     // Character Encoding Filter Bean
     characterEncodingFilter(org.springframework.web.filter.CharacterEncodingFilter) {
         encoding = "utf-8"
