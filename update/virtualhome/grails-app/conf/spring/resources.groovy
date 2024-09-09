ed904ace82c57dfb5fd23bd5424c2e0b2f3420bc
Jamie Getty <jamie.getty@reannz.co.nz>
2024-09-09 16:54:01 +1200

    new: Migrate spring beans to correct location

--- update/virtualhome/grails-app/conf/spring/resources.groovy
+++ update/virtualhome/grails-app/conf/spring/resources.groovy
@@ -1,3 +1,29 @@
-// Place your Spring DSL code here
 beans = {
+    // Grails Application Factory Bean
+    grailsApplication(org.codehaus.groovy.grails.commons.GrailsApplicationFactoryBean) {
+        description = "Grails application factory bean"
+        grailsDescriptor = "/WEB-INF/grails.xml"
+    }
+
+    // Plugin Manager Bean
+    pluginManager(org.codehaus.groovy.grails.plugins.GrailsPluginManagerFactoryBean) {
+        description = "A bean that manages Grails plugins"
+        grailsDescriptor = "/WEB-INF/grails.xml"
+        application = ref("grailsApplication")
+    }
+
+    // Grails Runtime Configurator Bean
+    grailsConfigurator(org.codehaus.groovy.grails.commons.spring.GrailsRuntimeConfigurator) {
+        pluginManager = ref("pluginManager")
+        // Constructor arg
+        constructorArgs = [ref("grailsApplication")]
+    }
+
+    // Character Encoding Filter Bean
+    characterEncodingFilter(org.springframework.web.filter.CharacterEncodingFilter) {
+        encoding = "utf-8"
+    }
+
+    // Conversion Service Bean
+    conversionService(org.springframework.context.support.ConversionServiceFactoryBean)
 }
