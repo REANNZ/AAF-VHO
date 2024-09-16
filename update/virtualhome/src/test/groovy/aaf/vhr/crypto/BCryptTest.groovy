9f1303ea2df33d1837b5055bd1e0abacec240610
Jamie Getty <jamie.getty@reannz.co.nz>
2024-09-16 16:50:21 +1200

    new: Make BCryptTest more groovy-like

--- update/virtualhome/src/test/groovy/aaf/vhr/crypto/BCryptTest.groovy
+++ update/virtualhome/src/test/groovy/aaf/vhr/crypto/BCryptTest.groovy
@@ -14,8 +14,6 @@ package aaf.vhr.crypto;
 // ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
 // OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
 
-import org.junit.Assert;
-
 import spock.lang.Specification;
 
 /**
@@ -24,69 +22,29 @@ import spock.lang.Specification;
  * @version 0.2
  */
 
-public class BCryptTest extends Specification {
-	String test_vectors[][] = {
-			{ "", 
-			"$2a$06$DCq7YPn5Rq63x1Lad4cll.",
-			"$2a$06$DCq7YPn5Rq63x1Lad4cll.TV4S6ytwfsfvkgY8jIucDrjc8deX1s." },
-			{ "",
-			"$2a$08$HqWuK6/Ng6sg9gQzbLrgb.",
-			"$2a$08$HqWuK6/Ng6sg9gQzbLrgb.Tl.ZHfXLhvt/SgVyWhQqgqcZ7ZuUtye" },
-			{ "",
-			"$2a$10$k1wbIrmNyFAPwPVPSVa/ze",
-			"$2a$10$k1wbIrmNyFAPwPVPSVa/zecw2BCEnBwVS2GbrmgzxFUOqW9dk4TCW" },
-			{ "",
-			"$2a$12$k42ZFHFWqBp3vWli.nIn8u",
-			"$2a$12$k42ZFHFWqBp3vWli.nIn8uYyIkbvYRvodzbfbK18SSsY.CsIQPlxO" },
-			{ "a",
-			"$2a$06$m0CrhHm10qJ3lXRY.5zDGO",
-			"$2a$06$m0CrhHm10qJ3lXRY.5zDGO3rS2KdeeWLuGmsfGlMfOxih58VYVfxe" },
-			{ "a", 
-			"$2a$08$cfcvVd2aQ8CMvoMpP2EBfe",
-			"$2a$08$cfcvVd2aQ8CMvoMpP2EBfeodLEkkFJ9umNEfPD18.hUF62qqlC/V." },
-			{ "a",
-			"$2a$10$k87L/MF28Q673VKh8/cPi.",
-			"$2a$10$k87L/MF28Q673VKh8/cPi.SUl7MU/rWuSiIDDFayrKk/1tBsSQu4u" },
-			{ "a",
-			"$2a$12$8NJH3LsPrANStV6XtBakCe",
-			"$2a$12$8NJH3LsPrANStV6XtBakCez0cKHXVxmvxIlcz785vxAIZrihHZpeS" },
-			{ "abc",
-			"$2a$06$If6bvum7DFjUnE9p2uDeDu",
-			"$2a$06$If6bvum7DFjUnE9p2uDeDu0YHzrHM6tf.iqN8.yx.jNN1ILEf7h0i" },
-			{ "abc",
-			"$2a$08$Ro0CUfOqk6cXEKf3dyaM7O",
-			"$2a$08$Ro0CUfOqk6cXEKf3dyaM7OhSCvnwM9s4wIX9JeLapehKK5YdLxKcm" },
-			{ "abc",
-			"$2a$10$WvvTPHKwdBJ3uk0Z37EMR.",
-			"$2a$10$WvvTPHKwdBJ3uk0Z37EMR.hLA2W6N9AEBhEgrAOljy2Ae5MtaSIUi" },
-			{ "abc",
-			"$2a$12$EXRkfkdmXn2gzds2SSitu.",
-			"$2a$12$EXRkfkdmXn2gzds2SSitu.MW9.gAVqa9eLS1//RYtYCmB1eLHg.9q" },
-			{ "abcdefghijklmnopqrstuvwxyz",
-			"$2a$06$.rCVZVOThsIa97pEDOxvGu",
-			"$2a$06$.rCVZVOThsIa97pEDOxvGuRRgzG64bvtJ0938xuqzv18d3ZpQhstC" },
-			{ "abcdefghijklmnopqrstuvwxyz",
-			"$2a$08$aTsUwsyowQuzRrDqFflhge",
-			"$2a$08$aTsUwsyowQuzRrDqFflhgekJ8d9/7Z3GV3UcgvzQW3J5zMyrTvlz." },
-			{ "abcdefghijklmnopqrstuvwxyz",
-			"$2a$10$fVH8e28OQRj9tqiDXs1e1u",
-			"$2a$10$fVH8e28OQRj9tqiDXs1e1uxpsjN0c7II7YPKXua2NAKYvM6iQk7dq" },
-			{ "abcdefghijklmnopqrstuvwxyz",
-			"$2a$12$D4G5f18o7aMMfwasBL7Gpu",
-			"$2a$12$D4G5f18o7aMMfwasBL7GpuQWuP3pkrZrOAnqP.bmezbMng.QwJ/pG" },
-			{ "~!@#$%^&*()      ~!@#$%^&*()PNBFRD",
-			"$2a$06$fPIsBO8qRqkjj273rfaOI.",
-			"$2a$06$fPIsBO8qRqkjj273rfaOI.HtSV9jLDpTbZn782DC6/t7qT67P6FfO" },
-			{ "~!@#$%^&*()      ~!@#$%^&*()PNBFRD",
-			"$2a$08$Eq2r4G/76Wv39MzSX262hu",
-			"$2a$08$Eq2r4G/76Wv39MzSX262huzPz612MZiYHVUJe/OcOql2jo4.9UxTW" },
-			{ "~!@#$%^&*()      ~!@#$%^&*()PNBFRD",
-			"$2a$10$LgfYWkbzEvQ4JakH7rOvHe",
-			"$2a$10$LgfYWkbzEvQ4JakH7rOvHe0y8pHKF9OaFgwUZ2q7W2FFZmZzJYlfS" },
-			{ "~!@#$%^&*()      ~!@#$%^&*()PNBFRD",
-			"$2a$12$WApznUOJfkEGSmYRfnkrPO",
-			"$2a$12$WApznUOJfkEGSmYRfnkrPOr466oFDCaj4b6HY3EXGvfxm43seyhgC" },
-		};
+public class BCryptSpec extends Specification {
+	def test_vectors = [
+			[ "", "\$2a\$06\$DCq7YPn5Rq63x1Lad4cll.", "\$2a\$06\$DCq7YPn5Rq63x1Lad4cll.TV4S6ytwfsfvkgY8jIucDrjc8deX1s." ],
+			[ "", "\$2a\$08\$HqWuK6/Ng6sg9gQzbLrgb.", "\$2a\$08\$HqWuK6/Ng6sg9gQzbLrgb.Tl.ZHfXLhvt/SgVyWhQqgqcZ7ZuUtye" ],
+			[ "". "\$2a\$10\$k1wbIrmNyFAPwPVPSVa/ze", "\$2a\$10\$k1wbIrmNyFAPwPVPSVa/zecw2BCEnBwVS2GbrmgzxFUOqW9dk4TCW" ],
+			[ "", "\$2a\$12\$k42ZFHFWqBp3vWli.nIn8u", "\$2a\$12\$k42ZFHFWqBp3vWli.nIn8uYyIkbvYRvodzbfbK18SSsY.CsIQPlxO" ],
+			[ "a", "\$2a\$06\$m0CrhHm10qJ3lXRY.5zDGO", "\$2a\$06\$m0CrhHm10qJ3lXRY.5zDGO3rS2KdeeWLuGmsfGlMfOxih58VYVfxe" ],
+			[ "a",  "\$2a\$08\$cfcvVd2aQ8CMvoMpP2EBfe", "\$2a\$08\$cfcvVd2aQ8CMvoMpP2EBfeodLEkkFJ9umNEfPD18.hUF62qqlC/V." ],
+			[ "a", "\$2a\$10\$k87L/MF28Q673VKh8/cPi.", "\$2a\$10\$k87L/MF28Q673VKh8/cPi.SUl7MU/rWuSiIDDFayrKk/1tBsSQu4u" ],
+			[ "a", "\$2a\$12\$8NJH3LsPrANStV6XtBakCe", "\$2a\$12\$8NJH3LsPrANStV6XtBakCez0cKHXVxmvxIlcz785vxAIZrihHZpeS" ],
+			[ "abc", "\$2a\$06\$If6bvum7DFjUnE9p2uDeDu", "\$2a\$06\$If6bvum7DFjUnE9p2uDeDu0YHzrHM6tf.iqN8.yx.jNN1ILEf7h0i" ],
+			[ "abc", "\$2a\$08\$Ro0CUfOqk6cXEKf3dyaM7O", "\$2a\$08\$Ro0CUfOqk6cXEKf3dyaM7OhSCvnwM9s4wIX9JeLapehKK5YdLxKcm" ],
+			[ "abc", "\$2a\$10\$WvvTPHKwdBJ3uk0Z37EMR.", "\$2a\$10\$WvvTPHKwdBJ3uk0Z37EMR.hLA2W6N9AEBhEgrAOljy2Ae5MtaSIUi" ],
+			[ "abc", "\$2a\$12\$EXRkfkdmXn2gzds2SSitu.", "\$2a\$12\$EXRkfkdmXn2gzds2SSitu.MW9.gAVqa9eLS1//RYtYCmB1eLHg.9q" ],
+			[ "abcdefghijklmnopqrstuvwxyz", "\$2a\$06\$.rCVZVOThsIa97pEDOxvGu", "\$2a\$06\$.rCVZVOThsIa97pEDOxvGuRRgzG64bvtJ0938xuqzv18d3ZpQhstC" ],
+			[ "abcdefghijklmnopqrstuvwxyz", "\$2a\$08\$aTsUwsyowQuzRrDqFflhge", "\$2a\$08\$aTsUwsyowQuzRrDqFflhgekJ8d9/7Z3GV3UcgvzQW3J5zMyrTvlz." ],
+			[ "abcdefghijklmnopqrstuvwxyz", "\$2a\$10\$fVH8e28OQRj9tqiDXs1e1u", "\$2a\$10\$fVH8e28OQRj9tqiDXs1e1uxpsjN0c7II7YPKXua2NAKYvM6iQk7dq" ],
+			[ "abcdefghijklmnopqrstuvwxyz", "\$2a\$12\$D4G5f18o7aMMfwasBL7Gpu", "\$2a\$12\$D4G5f18o7aMMfwasBL7GpuQWuP3pkrZrOAnqP.bmezbMng.QwJ/pG" ],
+			[ "~!@#\$%^&*()      ~!@#\$%^&*()PNBFRD", "\$2a\$06\$fPIsBO8qRqkjj273rfaOI.", "\$2a\$06\$fPIsBO8qRqkjj273rfaOI.HtSV9jLDpTbZn782DC6/t7qT67P6FfO" ],
+			[ "~!@#\$%^&*()      ~!@#\$%^&*()PNBFRD", "\$2a\$08\$Eq2r4G/76Wv39MzSX262hu", "\$2a\$08\$Eq2r4G/76Wv39MzSX262huzPz612MZiYHVUJe/OcOql2jo4.9UxTW" ],
+			[ "~!@#\$%^&*()      ~!@#\$%^&*()PNBFRD", "\$2a\$10\$LgfYWkbzEvQ4JakH7rOvHe", "\$2a\$10\$LgfYWkbzEvQ4JakH7rOvHe0y8pHKF9OaFgwUZ2q7W2FFZmZzJYlfS" ],
+			[ "~!@#\$%^&*()      ~!@#\$%^&*()PNBFRD", "\$2a\$12\$WApznUOJfkEGSmYRfnkrPO", "\$2a\$12\$WApznUOJfkEGSmYRfnkrPOr466oFDCaj4b6HY3EXGvfxm43seyhgC" ],
+	];
 
 	/**
 	 * Entry point for unit tests
@@ -99,101 +57,97 @@ public class BCryptTest extends Specification {
 	/**
 	 * Test method for 'BCrypt.hashpw(String, String)'
 	 */
-	public void testHashpw() {
-		System.out.print("BCrypt.hashpw(): ");
-		for (int i = 0; i < test_vectors.length; i++) {
-			String plain = test_vectors[i][0];
-			String salt = test_vectors[i][1];
-			String expected = test_vectors[i][2];
-			String hashed = BCrypt.hashpw(plain, salt);
-			Assert.assertEquals(hashed, expected);
-			System.out.print(".");
+	def 'testHashpw'() {
+		for (test_vector in test_vectors) {
+			when:
+			def plain = test_vector[0]
+			def salt = test_vector[1]
+			def expected = test_vector[2]
+			def hashed = BCrypt.hashpw(plain, salt)
+
+			expect:
+			hashed == expected
 		}
-		System.out.println("");
 	}
 
 	/**
 	 * Test method for 'BCrypt.gensalt(int)'
 	 */
-	public void testGensaltInt() {
-		System.out.print("BCrypt.gensalt(log_rounds):");
-		for (int i = 4; i <= 12; i++) {
-			System.out.print(" " + Integer.toString(i) + ":");
-			for (int j = 0; j < test_vectors.length; j += 4) {
-				String plain = test_vectors[j][0];
-				String salt = BCrypt.gensalt(i);
-				String hashed1 = BCrypt.hashpw(plain, salt);
-				String hashed2 = BCrypt.hashpw(plain, hashed1);
-				Assert.assertEquals(hashed1, hashed2);
-				System.out.print(".");
+	def 'testGensaltInt'() {
+		for (def i = 4;  i <= 12 ; i++ ) {
+			for (def j = 0; j < test_vectors.length ; j+= 4) {
+				when:
+				def plain = test_vectors[j][0]
+				def salt = BCrypt.gensalt(i)
+				def hashed1 = BCrypt.hashpw(plain, salt)
+				def hashed2 = BCrypt.hashpw(plain, hashed1)
+
+				expect:
+				hashed1 == hashed2
 			}
 		}
-		System.out.println("");
 	}
 
 	/**
 	 * Test method for 'BCrypt.gensalt()'
 	 */
-	public void testGensalt() {
-		System.out.print("BCrypt.gensalt(): ");
-		for (int i = 0; i < test_vectors.length; i += 4) {
-			String plain = test_vectors[i][0];
-			String salt = BCrypt.gensalt();
-			String hashed1 = BCrypt.hashpw(plain, salt);
-			String hashed2 = BCrypt.hashpw(plain, hashed1);
-			Assert.assertEquals(hashed1, hashed2);
-			System.out.print(".");
+	def 'testGensalt'() {
+		for (def i = 0; i < test_vectors.length ; i += 4) {
+			when:
+			def plain = test_vectors[i][0]
+			def salt = BCrypt.gensalt();
+			def hashed1 = BCrypt.hashpw(plain, salt)
+			def hashed2 = BCrypt.hashpw(plain, hashed1)
+
+			expect:
+			hashed1 == hashed2
 		}
-		System.out.println("");
 	}
 
 	/**
 	 * Test method for 'BCrypt.checkpw(String, String)'
 	 * expecting success
 	 */
-	public void testCheckpw_success() {
-		System.out.print("BCrypt.checkpw w/ good passwords: ");
-		for (int i = 0; i < test_vectors.length; i++) {
-			String plain = test_vectors[i][0];
-			String expected = test_vectors[i][2];
-			Assert.assertTrue(BCrypt.checkpw(plain, expected));
-			System.out.print(".");
+	def 'testCheckpw_success'() {
+		for (test_vector in test_vectors) {
+			when:
+			def plain = test_vector[0]
+			def expected = test_vector[2]
+
+			expect:
+			BCrypt.checkpw(plain, expected)
 		}
-		System.out.println("");
 	}
 
 	/**
 	 * Test method for 'BCrypt.checkpw(String, String)'
 	 * expecting failure
 	 */
-	public void testCheckpw_failure() {
-		System.out.print("BCrypt.checkpw w/ bad passwords: ");
-		for (int i = 0; i < test_vectors.length; i++) {
-			int broken_index = (i + 4) % test_vectors.length;
-			String plain = test_vectors[i][0];
-			String expected = test_vectors[broken_index][2];
-			Assert.assertFalse(BCrypt.checkpw(plain, expected));
-			System.out.print(".");
+	def 'testCheckpw_failure'() {
+		for (test_vector in test_vectors) {
+			when:
+			def plain = test_vector[0]
+			def broken_index = (i + 4) % test_vectors.length
+			def expected = test_vectors[broken_index][2]
+
+			expect:
+			!BCrypt.checkpw(plain, expected)
 		}
-		System.out.println("");
 	}
 
 	/**
 	 * Test for correct hashing of non-US-ASCII passwords
 	 */
-	public void testInternationalChars() {
-		System.out.print("BCrypt.hashpw w/ international chars: ");
-		String pw1 = "ππππππππ";
-		String pw2 = "????????";
+	def 'testInternationalChars'() {
+		when:
+		def pw1 = "ππππππππ";
+		def pw2 = "????????";
+		def h1 = BCrypt.hashpw(pw1, BCrypt.gensalt());
+		def h2 = BCrypt.hashpw(pw2, BCrypt.gensalt());
 
-		String h1 = BCrypt.hashpw(pw1, BCrypt.gensalt());
-		Assert.assertFalse(BCrypt.checkpw(pw2, h1));
-		System.out.print(".");
-
-		String h2 = BCrypt.hashpw(pw2, BCrypt.gensalt());
-		Assert.assertFalse(BCrypt.checkpw(pw1, h2));
-		System.out.print(".");
-		System.out.println("");
+		expect:
+		!BCrypt.checkpw(pw2, h1)
+		!BCrypt.checkpw(pw1, h2)
 	}
 
 }
