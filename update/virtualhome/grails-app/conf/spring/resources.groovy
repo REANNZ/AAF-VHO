cadda887bad9059be3b1cfc1af7846b19448df01
Jamie Getty <jamie.getty@reannz.co.nz>
2024-09-17 16:10:35 +1200

    new: Migrate spring configuration

--- update/virtualhome/grails-app/conf/spring/resources.groovy
+++ update/virtualhome/grails-app/conf/spring/resources.groovy
@@ -12,5 +12,34 @@ beans = {
     urlPatterns = ['/*']
   }
 
+  // Port of grailsApplication from applicationContext.xml
+  grailsApplication(org.codehaus.groovy.grails.commons.GrailsApplicationFactoryBean) {
+      description = "Grails application factory bean"
+      grailsDescriptor = "/WEB-INF/grails.xml"
+  }
+
+  // Port of grailsConfigurator from applicationContext.xml
+  grailsConfigurator(org.codehaus.groovy.grails.commons.spring.GrailsRuntimeConfigurator) {
+      pluginManager = ref("pluginManager")
+      // Constructor arg
+      constructorArgs = [ref("grailsApplication")]
+  }
+
+  // Port of characterEncodingFilter from applicationContext.xml
+  characterEncodingFilter(org.springframework.web.filter.CharacterEncodingFilter) {
+      encoding = "utf-8"
+  }
+
+  // Port of pluginManager from applicationContext.sml
+  pluginManager(org.codehaus.groovy.grails.plugins.GrailsPluginManagerFactoryBean) {
+      description = "A bean that manages Grails plugins"
+      grailsDescriptor = "/WEB-INF/grails.xml"
+      application = ref("grailsApplication")
+  }
+
+  // Port of conversionService from applicationContext.xml
+  conversionService(org.springframework.context.support.ConversionServiceFactoryBean)
+
+
   shiroAuthenticationStrategy(aaf.base.shiro.FirstExceptionStrategy)
 }
