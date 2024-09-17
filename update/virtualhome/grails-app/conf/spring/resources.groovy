6ccb60a1bdb7b78e70ff98f62d05c69cf8e5fca8
Jamie Getty <jamie.getty@reannz.co.nz>
2024-09-17 14:12:39 +1200

    new: Move spring filter from web.xml to resources.groovy

--- update/virtualhome/grails-app/conf/spring/resources.groovy
+++ update/virtualhome/grails-app/conf/spring/resources.groovy
@@ -1,3 +1,16 @@
 beans = {
+
+  // Port of mulireadFilter from the old web.xml
+  filter('multireadFilter', aaf.base.util.http.MultiReadServletFilter) {
+    urlPatterns = ['/api/*']
+  }
+
+  // Port of charEncodingFilter from the old web.xml
+  filter('charEncodingFilter', org.springframework.web.filter.DelegatingFilterProxy) {
+    targetBeanName = 'charEncodingFilter'
+    targetFilterLifecycle = true
+    urlPatterns = ['/*']
+  }
+
   shiroAuthenticationStrategy(aaf.base.shiro.FirstExceptionStrategy)
 }
