90d09139f417c5bd583d3d141be7a26111f555bc
Jamie Getty <jamie.getty@reannz.co.nz>
2024-09-23 17:21:37 +1200

    new: Migrate spring beans

--- update/virtualhome/grails-app/conf/spring/resources.groovy
+++ update/virtualhome/grails-app/conf/spring/resources.groovy
@@ -1,46 +1,18 @@
+import org.springframework.web.filter.CharacterEncodingFilter
+import org.springframework.context.support.ConversionServiceFactoryBean
+import org.springframework.boot.web.servlet.FilterRegistrationBean
+
+import aaf.base.shiro.FirstExceptionStrategy
+
 beans = {
 
-  // Port of mulireadFilter from the old web.xml
-  filter('multireadFilter', aaf.base.util.http.MultiReadServletFilter) {
-    urlPatterns = ['/api/*']
-  }
-
-  // Port of charEncodingFilter from the old web.xml
-  filter('charEncodingFilter', org.springframework.web.filter.DelegatingFilterProxy) {
-    targetBeanName = 'charEncodingFilter'
-    targetFilterLifecycle = true
-    urlPatterns = ['/*']
-  }
-
-  // Port of grailsApplication from applicationContext.xml
-  /*grailsApplication(org.codehaus.groovy.grails.commons.GrailsApplicationFactoryBean) {
-      description = "Grails application factory bean"
-      grailsDescriptor = "/WEB-INF/grails.xml"
-  }*/
-
-  // Port of grailsConfigurator from applicationContext.xml
-  /*grailsConfigurator(org.codehaus.groovy.grails.commons.spring.GrailsRuntimeConfigurator) {
-      pluginManager = ref("pluginManager")
-      // Constructor arg
-      constructorArgs = [ref("grailsApplication")]
-  }*/
-
   // Port of characterEncodingFilter from applicationContext.xml
-  /*characterEncodingFilter(org.springframework.web.filter.CharacterEncodingFilter) {
-      encoding = "utf-8"
-  }*/
-
-  // Port of pluginManager from applicationContext.sml
-  /*pluginManager(org.codehaus.groovy.grails.plugins.GrailsPluginManagerFactoryBean) {
-      description = "A bean that manages Grails plugins"
-      grailsDescriptor = "/WEB-INF/grails.xml"
-      application = ref("grailsApplication")
-  }*/
-
+  characterEncodingFilter(CharacterEncodingFilter) {
+    encoding = 'utf-8'
+  }
 
   // Port of conversionService from applicationContext.xml
-  conversionService(org.springframework.context.support.ConversionServiceFactoryBean)
+  conversionService(ConversionServiceFactoryBean)
 
-
-  shiroAuthenticationStrategy(aaf.base.shiro.FirstExceptionStrategy)
+  shiroAuthenticationStrategy(FirstExceptionStrategy)
 }
