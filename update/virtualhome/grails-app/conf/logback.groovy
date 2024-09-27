53f75ca1d86faeb9e9fb5530e466a699d008616a
Jamie Getty <jamie.getty@reannz.co.nz>
2024-09-27 17:12:24 +1200

    new: Port log4j configuration

--- update/virtualhome/grails-app/conf/logback.groovy
+++ update/virtualhome/grails-app/conf/logback.groovy
@@ -1,36 +1,67 @@
-import grails.util.BuildSettings
-import grails.util.Environment
-import org.springframework.boot.logging.logback.ColorConverter
-import org.springframework.boot.logging.logback.WhitespaceThrowableProxyConverter
+import ch.qos.logback.classic.encoder.PatternLayoutEncoder
+import ch.qos.logback.core.rolling.RollingFileAppender
+import ch.qos.logback.core.rolling.TimeBasedRollingPolicy
 
-import java.nio.charset.Charset
-
-conversionRule 'clr', ColorConverter
-conversionRule 'wex', WhitespaceThrowableProxyConverter
-
-// See http://logback.qos.ch/manual/groovy.html for details on configuration
-appender('STDOUT', ConsoleAppender) {
+// Port of the 'app-security' appender from application_config.grooby
+appender('app-security-appender', RollingFileAppender) {
+    append = true
     encoder(PatternLayoutEncoder) {
-        charset = Charset.forName('UTF-8')
-
-        pattern =
-                '%clr(%d{yyyy-MM-dd HH:mm:ss.SSS}){faint} ' + // Date
-                        '%clr(%5p) ' + // Log level
-                        '%clr(---){faint} %clr([%15.15t]){faint} ' + // Thread
-                        '%clr(%-40.40logger{39}){cyan} %clr(:){faint} ' + // Logger
-                        '%m%n%wex' // Message
+        pattern = "%d{[ dd.MM.yy HH:mm:ss.SSS]} %-5p %c %x - %m%n"
+    }
+    rollingPolicy(TimeBasedRollingPolicy) {
+        FileNamePattern = "/opt/virtualhome/logs/app-security.log-yyyy-MM-dd"
     }
 }
 
-def targetDir = BuildSettings.TARGET_DIR
-if (Environment.isDevelopmentMode() && targetDir != null) {
-    appender("FULL_STACKTRACE", FileAppender) {
-        file = "${targetDir}/stacktrace.log"
-        append = true
-        encoder(PatternLayoutEncoder) {
-            pattern = "%level %logger - %msg%n"
-        }
+// Port of the 'app' appender from application_config.groovy
+appender('app-appender', RollingFileAppender) {
+    append = true
+    encoder(PatternLayoutEncoder) {
+        pattern = "%d{[ dd.MM.yy HH:mm:ss.SSS]} %-5p %c %x - %m%n"
     }
-    logger("StackTrace", ERROR, ['FULL_STACKTRACE'], false)
+    rollingPolicy(TimeBasedRollingPolicy) {
+        FileNamePattern = "/opt/virtualhome/logs/app.log-yyyy-MM-dd"
+    }
 }
-root(ERROR, ['STDOUT'])
+
+// Port of the 'app-grails' appender from application_config.groovy
+appender('app-grails-appender', RollingFileAppender) {
+    append = true
+    encoder(PatternLayoutEncoder) {
+        pattern = "%d{[ dd.MM.yy HH:mm:ss.SSS]} %-5p %c %x - %m%n"
+    }
+    rollingPolicy(TimeBasedRollingPolicy) {
+        FileNamePattern = "/opt/virtualhome/logs/app-grails.log-yyyy-MM-dd"
+    }
+}
+
+// Port of the 'stacktrace' appender from application_config.groovy
+appender('stacktrace-appender', RollingFileAppender) {
+    append = true
+    encoder(PatternLayoutEncoder) {
+        pattern = "%d{[ dd.MM.yy HH:mm:ss.SSS]} %-5p %c %x - %m%n"
+    }
+    rollingPolicy(TimeBasedRollingPolicy) {
+        FileNamePattern = "/opt/virtualhome/logs/app-stacktrace.log-yyyy-MM-dd"
+    }
+}
+
+// Port of the 'app-security' logger
+logger('app-security', INFO, ['app-security-appender'], false)
+
+// Port of the 'app' logger
+logger("grails.app.controllers", INFO, ['app-appender'], false)
+logger("grails.app.domains", INFO, ['app-appender'], false)
+logger("grails.app.services", INFO, ['app-appender'], false)
+logger("grails.app.realms", INFO, ['app-appender'], false)
+logger("aaf.vhr", INFO, ['app-appender'], false)
+logger("org.apache.shiro", INFO, ['app-appender'], false)
+
+// Port of the 'app-grails' logger
+logger("org.codehaus.groovy.grails.web.servlet", WARN, ['app-grails-appender'], false)
+logger("org.codehaus.groovy.grails.web.pages", WARN, ['app-grails-appender'], false)
+logger("org.codehaus.groovy.grails.web.sitemesh", WARN, ['app-grails-appender'], false)
+logger("org.codehaus.groovy.grails.web.mapping.filter", WARN, ['app-grails-appender'], false)
+logger("org.codehaus.groovy.grails.web.mapping", WARN, ['app-grails-appender'], false)
+logger("org.codehaus.groovy.grails.commons", WARN, ['app-grails-appender'], false)
+logger("org.codehaus.groovy.grails.plugins", WARN, ['app-grails-appender'], false)
