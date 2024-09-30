2206033079639357fd67fa4c953574a33bfdcb5e
Jamie Getty <jamie.getty@reannz.co.nz>
2024-10-01 11:26:35 +1300

    new: Migrate UrlMappings.groovy

--- update/virtualhome/grails-app/controllers/virtualhome/UrlMappings.groovy
+++ update/virtualhome/grails-app/controllers/virtualhome/UrlMappings.groovy
@@ -14,3 +14,122 @@ class UrlMappings {
         "404"(view:'/notFound')
     }
 }
+
+/*
+class UrlMappings {
+
+  static mappings = {
+
+    "/"(controller:"dashboard", action:"welcome")
+
+    "/login" {
+      controller="login"
+      action="index"
+    }
+    "/login/$action?" {
+      controller="login"
+    }
+
+    "/myaccount/setup/login-available" {
+      controller="finalization"
+      action="loginAvailable"
+    }
+    "/myaccount/setup/complete" {
+      controller="finalization"
+      action="complete"
+    }
+    "/myaccount/setup/used" {
+      controller="finalization"
+      action="used"
+    }
+    "/myaccount/setup/error" {
+      controller="finalization"
+      action="error"
+    }
+    "/myaccount/setup/$inviteCode" {
+      controller="finalization"
+    }
+    "/myaccount" {
+      controller="account"
+      action="index"
+    }
+    "/myaccount/login" {
+      controller="account"
+      action="login"
+    }
+    "/myaccount/twosteplogin" {
+      controller="account"
+      action="twosteplogin"
+    }
+    "/myaccount/logout" {
+      controller="account"
+      action="logout"
+    }
+    "/myaccount/details" {
+      controller="account"
+      action="show"
+    }
+    "/myaccount/changedetails" {
+      controller="account"
+      action="changedetails"
+    }
+    "/myaccount/completedetailschange" {
+      controller="account"
+      action="completedetailschange"
+    }
+    "/myaccount/enabletwostep" {
+      controller="account"
+      action="enabletwostep"
+    }
+    "/myaccount/finishenablingtwostep" {
+      controller="account"
+      action="finishenablingtwostep"
+    }
+
+    "/lostpassword/$action" {
+      controller="lostPassword"
+    }
+    "/lostusername/$action" {
+      controller="lostUsername"
+    }
+    "/migration/$action" {
+      controller="migrate"
+    }
+
+    "/dashboard"(controller:"dashboard", action:"dashboard")
+
+    "/accounts/list" {
+      controller="managedSubject"
+      action="list"
+    }
+    "/groups/list" {
+      controller="group"
+      action="list"
+    }
+
+    "/organisations/groups/accounts/$action/$id?" {
+      controller="managedSubject"
+    }
+    "/organisations/groups/accountstatus/$id" {
+      controller="group"
+      action="nonfinalized"
+    }
+    "/organisations/groups/$action/$id?" {
+      controller="group"
+    }
+    "/organisations/$action/$id?" {
+      controller="organization"
+    }
+
+    "/backend/manageadministrators/$action/$id?" {
+      controller="manageAdministrators"
+    }
+
+    "/api/v1/login/confirmsession/$sessionID" (controller: "loginApi", action: "confirmsession")
+    "/api/v1/login/basicauth" (controller: "loginApi", action:"basicauth")
+    "/api/v1/organizations/$action/$id?" (controller: "organizationApi")
+    "/api/v1/scopes/$action?" (controller: "scopesApi")
+  }
+
+}
+*/
