7fc9c4cbfa8db8b29f7e108c98b82aeb996a3521
Jamie Getty <jamie.getty@reannz.co.nz>
2024-10-03 15:07:07 +1300

    new: Fix compilation issues.

--- update/virtualhome/src/main/groovy/ShiroAuditResolver.groovy
+++ update/virtualhome/src/main/groovy/ShiroAuditResolver.groovy
@@ -1,3 +1,6 @@
+import org.springframework.stereotype.Component
+import grails.plugins.orm.auditable.resolvers.DefaultAuditRequestResolver
+
 // Used in place of the old actorClosure from Config.groovy
 @Component('auditRequestResolver')
 class ShiroAuditResolver extends DefaultAuditRequestResolver {
