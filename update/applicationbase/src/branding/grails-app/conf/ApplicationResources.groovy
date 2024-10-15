74ffafd3f6265bf4245f697e786808f85312e290
Jamie Getty <jamie.getty@reannz.co.nz>
2024-10-15 16:59:04 +1300

    new: Remove unused files

--- update/applicationbase/src/branding/grails-app/conf/ApplicationResources.groovy
+++
@@ -1,7 +0,0 @@
-modules = { 
-  'app' {
-    dependsOn 'app_base'
-    resource url: 'css/application.css', attrs:[rel:'stylesheet/less', type:'css']
-    resource url: 'js/application.js', attrs:[type:'js']
-  }
-}
