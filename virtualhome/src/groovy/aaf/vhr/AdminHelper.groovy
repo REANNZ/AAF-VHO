f3219464bc36c4f0ad52740e595dfd91df9bfa6f
Jamie Getty <jamie.getty@reannz.co.nz>
2024-11-25 15:34:09 +1300

    new: Improve AdminHelper

--- virtualhome/src/groovy/aaf/vhr/AdminHelper.groovy
+++ virtualhome/src/groovy/aaf/vhr/AdminHelper.groovy
@@ -2,6 +2,7 @@ package aaf.vhr
 
 import aaf.base.identity.Subject
 import aaf.vhr.Group
+import aaf.vhr.Organization
 import org.apache.shiro.SecurityUtils
 
 class AdminHelper {
@@ -12,11 +13,35 @@ class AdminHelper {
         subject.roles.each { role ->
             def roleComponents = role.name.split(':')
             if (roleComponents.size() == 3 && roleComponents[0] == "group" && roleComponents[2] == "administrators") {
-            def id = roleComponents[1] as Integer
-            adminList.add(Group.get(id))
+                def id = roleComponents[1] as Integer
+                adminList.add(Group.get(id))
             }
         }
 
         return adminList
+    }
+
+    static def getAdminOrganisations() {
+        def adminList = []
+
+        def subject = Subject.get(SecurityUtils.getSubject()?.getPrincipal())
+        subject.roles.each { role ->
+            def roleComponents = role.name.split(':')
+            if (roleComponents.size() == 3 && roleComponents[0] == "organization" && roleComponents[2] == "administrators") {
+                def id = roleComponents[1] as Integer
+                adminList.add(Organization.get(id))
+            }
+        }
+
+        return adminList
+    }
+
+    static def isOrganizationInsider(Integer organizationID) {
+        def orgGroups = Organization.get(organizationID).groups
+        def adminGroups = getAdminGroups()
+        if (orgGroups.intersect(adminGroups))
+            return true
+
+        return false
     }
 }
