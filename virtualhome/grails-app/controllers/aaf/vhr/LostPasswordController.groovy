d6e66f49263e2afa4908bf2da0d3c5bb618b3b32
Jamie Getty <jamie.getty@reannz.co.nz>
2025-12-03 16:46:26 +1300

    new: Ignore HEAD requests to obtainsubject to prevent multiple SMS messages

--- virtualhome/grails-app/controllers/aaf/vhr/LostPasswordController.groovy
+++ virtualhome/grails-app/controllers/aaf/vhr/LostPasswordController.groovy
@@ -9,7 +9,7 @@ import aaf.vhr.switchch.vho.DeprecatedSubject
 
 class LostPasswordController {
 
-  static allowedMethods = [emailed: 'POST', validatereset: 'POST']
+  static allowedMethods = [emailed: 'POST', validatereset: 'POST', obtainsubject: 'GET']
 
   final String CURRENT_USER = "aaf.vhr.LostPasswordController.CURRENT_USER"
   final String EMAIL_CODE_SUBJECT ='controllers.aaf.vhr.lostpassword.email.code.subject'
