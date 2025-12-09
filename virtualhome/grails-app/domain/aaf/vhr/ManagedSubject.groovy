2f2a889248ada31d43e7696dc68c66ff3a5e71c5
Jamie Getty <jamie.getty@reannz.co.nz>
2025-12-09 17:16:49 +1300

    new: Make regex more clear

--- virtualhome/grails-app/domain/aaf/vhr/ManagedSubject.groovy
+++ virtualhome/grails-app/domain/aaf/vhr/ManagedSubject.groovy
@@ -516,7 +516,7 @@ class ManagedSubject {
     }
 
     // Silently remove anything that might be entered by users in a valid-looking phone number.
-    checkedNumber = checkedNumber.replaceAll("[ .\\-()]", '')
+    checkedNumber = checkedNumber.replaceAll("[- .()]", '')
 
     // Valid phone numbers for the app only contain '+' and 0-9. Anything else is probably junk that users will be warned about.
     def newNumber = checkedNumber.replaceAll("[^0-9+]", '')
